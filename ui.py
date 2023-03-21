f4adf5538d25a108896acd72e362755ed977fbf8
Mateusz Szałapski <mateuszszalapski@MacBook-Air-Mateusz.local>
2023-03-21 18:24:12 +0100

    Plansza z otwartmi drzwiami

--- ui.py
+++ ui.py
@@ -7,7 +7,8 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 
 def display_board(board):
-    print("\u2584"*(len(board[0])+2))
+    north_wall = "\u2584"*(len(board[0])+2)
+    open_door(north_wall, 4) #zmiana rozmiaru drzwi
     for row in board:
         wall = "\u2588"
         for col in row:
@@ -17,6 +18,19 @@ def display_board(board):
         wall += "\u2588"
         print(wall)
     print("\u2580"*(len(board[0])+2))
+
+def open_door(wall, door_size = 4):
+    wall = list(wall)
+    half_wall = int(len(wall)  / 2) - int(door_size/2)
+    for i in range(half_wall, half_wall + door_size):
+        wall[i] = " "
+    wall = "".join(wall)    
+    print(wall)
+
+
+
+     
+
 
 
 
