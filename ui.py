6e5a64fa3028120d4ab98149697db60fe7ed7d7f
Mateusz Szałapski <mateuszszalapski@MacBook-Air-Mateusz.local>
2023-03-21 18:10:29 +0100

    Plansza bez drzwi

--- ui.py
+++ ui.py
@@ -1,4 +1,25 @@
+board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+#"\u2584" góra
+#wall = "\u2580"
+#"\u2588" boki
+
+
+
 def display_board(board):
+    print("\u2584"*(len(board[0])+2))
+    for row in board:
+        wall = "\u2588"
+        for col in row:
+            if col == 0:
+                insert = " "
+            wall += insert
+        wall += "\u2588"
+        print(wall)
+    print("\u2580"*(len(board[0])+2))
+
+
+
     '''
     Displays complete game board on the screen
 
@@ -6,3 +27,21 @@ def display_board(board):
     Nothing
     '''
     pass
+
+display_board(board)
+
+
+LEVEL1 = (''' 
+-------------------------------- 
+|                               |
+|                               |
+|                               |
+|                               |
+|                               |
+|                               |
+-------------------------------- 
+''')
+               
+#def uidisplay_board():
+
+#def engine.put_player_on_board:
