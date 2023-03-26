102e6ecf0e809f9e3cb21c8f68d55466898cea3b
RadekRo <r.roclawski@ankara.pl>
2023-03-26 19:27:03 +0200

    Deleted unused commented functions

--- main.py
+++ main.py
@@ -66,103 +66,6 @@ def show_player_class_description(user_name, user_class):
 #     dot = "."*dots
 #     space = " "*spaces
 #     print(f"\n{space}Xx{dot}xXx{dot}xX")
-    
-       
-
-
-# def player_stats(player):
-    
-    
-#     ASPIRANT = {
-#         "HEALTH": 50,
-#         "STRENGTH": 30,
-#         "MANA": 20,
-#     }
-    
-#     STUDENT = {
-#         "HEALTH": 50,
-#         "STRENGTH": 25,
-#         "MANA": 15,
-#     }
-    
-#     MENTOR = {
-#         "HEALTH": 50,
-#         "STRENGTH": 15,
-#         "MANA": 10,
-#     }
-    
-    
-#     if player == "K" or player == "KANDYDAT":
-#         return ASPIRANT
-#     elif player == "S" or player == "STUDENT":
-#         return STUDENT
-#     elif player == "M" or player == "MENTOR":
-#         return MENTOR
-    
-    
-# def print_statistics(statistics):
-#     space = " "*25
-#     for i, c in statistics.items():
-#         print(space + i,c)
-    
-
-# def player_class(name_class, name, stats):
-#     space = " "*23
-#     if name_class == "K" or name_class == "KANDYDAT":
-#         print(f"{space}{name} WYBRAŁEŚ/ŁAS KANDYDATA!")
-#         draw_line(16,14)
-#         print("""
-#               To ambitny i pełen wigou osobnik, 
-#               który nie może doczekać się nowych
-#               wyzwań!
-              
-#               Zawsze też w wolnej chwili wymyka się 
-#               do pobliskiej karczmy, by posłuchać 
-#               nowych ciekawych histori i wypieć kufel 
-#               zimnego piwa!
-              
-#                         TWOJE STATYSTYKI!
-#               """)
-#         print_statistics(stats)
-#         draw_line(16,14)
-        
-#     elif name_class == "S" or name_class == "STUDENT":
-#         print(f"{space}{name} WYBRAŁEŚ STUDENTA!")
-#         draw_line(16,14)
-#         print("""
-#             To już nie co doświadczony w bojach osobnik, 
-#             rozwarzniej dobierajacy ekwipunek i studiujący 
-#             dokładniej mampy niezdobytych jeszcze przez 
-#             niego terenów.
-            
-#             Po mimo jego roztropności, świat ciągle 
-#             go zadziwia i stawia przed nim nowe wyzwania.
-            
-#                         TWOJE STATYSTYKI!
-#               """)
-#         print_statistics(stats)
-#         draw_line(16,14)
-        
-#     elif name_class == "M" or name_class == "MENTOR":
-#         print(f"{space}{name} WYBRAŁEŚ MENTORA!")
-#         draw_line(16,14)
-#         print("""
-#               Jego liczne blizny światczą o wielu bojach,
-#               o których chętnie opowiada, ucząć co gorliwszych 
-#               by bardziej roztropnie stawiali swoje kroki w 
-#               świecie przygody. 
-              
-#               Po mimo wielkiej wiedzy i on czasem musi się 
-#               zatrzymać i pomedytować nad ogniskiem by 
-#               doznać inspiracji.
-              
-#                         TWOJE STATYSTYKI!
-#               """)
-#         print_statistics(stats)
-#         draw_line(16,14)
-        
-# def HP_Player():
-#     pass
 
 def get_input_validation(user_input, type):
     match type:
@@ -228,29 +131,7 @@ def create_player():
     show_player_class_description(player["name"], player["class"])
 
     return player
-
-    # run = True
-    
-    # while run:
-    #     draw_line(15,0)
-    #     name = str(input("Wpisz swoje miano: ").upper())
-    #     if not all(x.isalpha or x == "" for x in  name):
-    #         print("Spróbuj ponownie.")
-    #     else:
-    #         print(name)
-    #     util.clear_screen()
-    #     draw_line(30,0)
-    #     print(f"Kim jesteś {name}? Klasy do wyboru [KANDYDAT, STUDENT, MENTOR]")
-    #     print("Możesz wpisać pełną nazwę lub, tylko pierwszą literę.")
-    #     name_class = input("Wybierz Klasę! ").upper()
-    #     util.clear_screen()
-    #     stats = player_stats(name_class)
-    #     player_class(name_class, name, stats) #print funkcji, która sama robi printa zwróci None poza tym, co ma być wydrukowane
-    # #else:
-    # #    run = False 
      
-
-
 def main():
     player = create_player()
     #board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
