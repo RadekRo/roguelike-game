b7741968bb6b2098fc9a5cd03a94bea238172b65
Radek Rocławski <r.roclawski@ankara.pl>
2023-03-28 14:25:47 +0200

    Made basic move_player function. Tested in WASD and I cases. Works fine. Ready to implement player movement options

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-import random, csv, os
+from util import key_pressed
 
 def create_board(width, height):
     board = list()
@@ -14,3 +14,23 @@ def create_board(width, height):
 def put_player_on_board(board, player):
     board[player["coordinates"][0]][player["coordinates"][1]] = player["icon"]
     return board
+
+def move_player():
+    while True:
+        key = key_pressed()
+        match key:
+            case "w":
+                print("player moves up")
+            case "s":
+                print("player moved down")
+            case "a":
+                print("player moved left")
+            case "d":
+                print("player moved right")
+            case "i":
+                print("inventory opened")
+            case _:
+                print("unknown command")
+                break
+move_player()
+
